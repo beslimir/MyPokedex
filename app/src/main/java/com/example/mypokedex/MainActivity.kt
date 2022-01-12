eb0f6c2c8b039451875923c48ea05cf9b8025b3b
beslimir <marin.beslo@gmail.com>
2022-01-12 10:29:23 +0100

    Dagger Hilt and Timber setup

--- app/src/main/java/com/example/mypokedex/MainActivity.kt
+++ app/src/main/java/com/example/mypokedex/MainActivity.kt
@@ -3,20 +3,17 @@ package com.example.mypokedex
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.example.mypokedex.ui.theme.MyPokedexTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/example/mypokedex/PokedexApp.kt
@@ -0,0 +1,15 @@
+package com.example.mypokedex
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class PokedexApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+
+}
